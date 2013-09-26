78e42799fe47fb0d6b05b0f7a1a46da6090a6438
Dmishin <shintyakov@gmail.com>
2013-09-26 23:53:08 +0400

    move minified bundle to the scripts/ folder

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ set -e
 echo Deployment script
 ODIR=../js-revca-deploy/js-revca
 
-cp -r images *.html bundle.js scripts-src *.css LICENSE README.md $ODIR
+cp -r images *.html scripts-src *.css LICENSE README.md $ODIR
+cp -r scripts/minified.js $ODIR/scripts
 
 cd $ODIR
 
