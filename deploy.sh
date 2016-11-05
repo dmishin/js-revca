5274ffa9c74a481b7f1eeb7099865ada7ae134b2
Dmitry Shintyakov <shintyakov@gmail.com>
2016-11-05 20:41:56 +0300

    enh: change publishing method

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-echo Deployment script
-ODIR=../js-revca-deploy/js-revca
+echo ===========================
+echo ==  Publishing JS-revca  ==
+echo ===========================
+
+ODIR=../homepage-sources/src/js-revca
 
 cp -r *.svg images *.html scripts-src *.css LICENSE README.md $ODIR
 cp -r scripts/revca_singlerot.js scripts/minified.js $ODIR/scripts
 
+
+echo =============================================
+echo ==  Done building, now publishing on site  ==
+echo =============================================
+
 cd $ODIR
-
-git add -A
-git status
-
-git commit -m "Automatic commit"
-
-git push
+sh ./publish.sh
